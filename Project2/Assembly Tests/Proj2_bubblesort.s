3e0225ab778fc47c1d117ef42ba90bd01c814fee
anorris <anorris@linuxvdi-36.ece.iastate.edu>
2024-04-12 17:16:22 -0500

    backup branch, has origional workflow

--- Project2/Assembly Tests/Proj2_bubblesort.s
+++ Project2/Assembly Tests/Proj2_bubblesort.s
@@ -25,23 +25,22 @@ numbers: .word 3, 0, 0, 3, 54, -1, 2, 256, -3, 0		# Array of N=10
 .text
 main:
 	
-	li $s0, 1    	#SwappedSomething = true	#initialize swapped something to true.
+	addiu $s0, $zero, 1    	#SwappedSomething = true	#initialize swapped something to true.
 	li $s2, 9 	#(n-2)				#initzalize for loop counter to stop at N-2, becasue of i and i++
 	
 	j whileloop	#start looping
 
 whileloop:
-	li $s1, 0	#i = 0				#initialize for loop counter to 0.
+	addiu, $s1, $zero, 0	#i = 0				#initialize for loop counter to 0.
 	nop		#nop or 28, $s0, 35
 	beq $s0, 0, fin	# we Need to run this only if swappedsomething == true, else done and print.
 	
 	nop
 	nop
 	nop		#5 nops because we need to know if we are branching or not before loading the next instruciton from pc+4 or the new address.
-	nop
-	nop
+
 	
-	li $s0, 0		#swapped something = false
+	addiu $s0, $zero, 0		#swapped something = false
 	lasw $s7, numbers					#(PsuedoInstruction) load memory address for array into $s7 with 3 additional nops
 	
 forloop:
@@ -51,13 +50,12 @@ forloop:
 	nop
 	nop
 	nop
-	nop
+
 	
 	bgt $t0, $t1, swap	#if numbers in i > numbers in i+1, need to sort.
 	
-	nop
+	addiu $s1, $s1, 1		#increment counter/i++ moved from lower because no matter what we need to inc
 	nop			#4 nops to allow us to not load bad data if we didnt branch.
-	nop
 	nop
 	
 	
@@ -66,21 +64,20 @@ forloop:
 	nop
 	nop
 	nop			#4 NOPS TO NOT LOAD BAD DATA BECAUSE OF JUMP, we want to wait until the jump adress is given back to the pc.
-	nop
+
 
 
 swap:
-	li $s0, 1		#swapped something == true.			#swap memory location $t0 with $t1
+	addiu $s0, $zero, 1		#swapped something == true.			#swap memory location $t0 with $t1
 	sw $t0, 4($s7)		#memory for numbers[i] = numbers[i+1]
 	sw $t1, 0($s7)		#memory numbers i+1 = numbers[i]
-increment:	
+increment:
+	##increment was here	
 	addiu $s7, $s7, 4		#increment memory location to next word(4 bytes or sll by 2.)
-	addiu $s1, $s1, 1		#increment counter/i++
+	
 	
 	nop
-	nop
-	nop			#4 nops to allow s1 to write.
-	nop
+	nop			#2 nops to allow s1 to write
 	
 	beq $s1, $s2, whileloop		#restart while loop if we are at the end of the for loop.
 	j forloop			#else do next iteration of for loop.
@@ -88,11 +85,8 @@ increment:
 	nop
 	nop
 	nop
-	nop
+
 	
 fin:	
-	nop
-	nop
-	nop
-	nop
+
 	halt			# end the program.
