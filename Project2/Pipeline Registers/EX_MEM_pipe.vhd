1be38a897ce086b8ad95e91440527f604764f168
dkearns <dkearns@iastate.edu>
2024-04-13 17:18:47 -0500

    changed signal names to not have _C

--- Project2/Pipeline Registers/EX_MEM_pipe.vhd
+++ Project2/Pipeline Registers/EX_MEM_pipe.vhd
@@ -18,14 +18,14 @@ entity EX_MEM_pipe is
         i_CLK           : in std_logic;                     -- Clock input
         i_RST           : in std_logic;                     -- Reset input
         i_WE            : in std_logic;                     -- Write enable
-        i_EX_halt_c     : in std_logic;                     -- Halt control signal
-        i_EX_MemToReg_c : in std_logic_vector(1 downto 0);  -- MemToReg control signal
-        i_EX_MemWrite_c : in std_logic;                     -- Memory write control signal
-        i_EX_RegWrite_c : in std_logic;                     -- Register write control signal
-        i_EX_Jump_c     : in std_logic;                     -- Jump control signal
+        i_EX_halt       : in std_logic;                     -- Halt control signal
+        i_EX_MemToReg   : in std_logic_vector(1 downto 0);  -- MemToReg control signal
+        i_EX_MemWrite   : in std_logic;                     -- Memory write control signal
+        i_EX_RegWrite   : in std_logic;                     -- Register write control signal
+        i_EX_Jump       : in std_logic;                     -- Jump control signal
         i_EX_ext_ctl    : in std_logic;                     -- Sign extension control signal
-        i_EX_jal_C      : in std_logic;                     -- Jump and link write back control signal
-        i_EX_jr_c       : in std_logic;                     -- Jump return control signal
+        i_EX_jal        : in std_logic;                     -- Jump and link write back control signal
+        i_EX_jr         : in std_logic;                     -- Jump return control signal
         i_EX_branch     : in std_logic;                     -- Branch output from ALU
         i_EX_PCP4       : in std_logic_vector(31 downto 0); -- PC+4 value
         i_EX_rs_data_o  : in std_logic_vector(31 downto 0); -- Output from Rs address
@@ -37,14 +37,14 @@ entity EX_MEM_pipe is
         ------------------------------------------------------------------------------------
         -- outputs
         ------------------------------------------------------------------------------------
-        o_MEM_halt_c     : out std_logic;                     -- Halt control signal
-        o_MEM_MemToReg_c : out std_logic_vector(1 downto 0);  -- MemToReg control signal
-        o_MEM_MemWrite_c : out std_logic;                     -- Memory write control signal
-        o_MEM_RegWrite_c : out std_logic;                     -- Register write control signal
-        o_MEM_Jump_c     : out std_logic;                     -- Jump control signal
+        o_MEM_halt       : out std_logic;                     -- Halt control signal
+        o_MEM_MemToReg   : out std_logic_vector(1 downto 0);  -- MemToReg control signal
+        o_MEM_MemWrite   : out std_logic;                     -- Memory write control signal
+        o_MEM_RegWrite   : out std_logic;                     -- Register write control signal
+        o_MEM_Jump       : out std_logic;                     -- Jump control signal
         o_MEM_ext_ctl    : out std_logic;                     -- Sign extension control signal
-        o_MEM_jal_C      : out std_logic;                     -- Jump and link write back control signal
-        o_MEM_jr_c       : out std_logic;                     -- Jump return control signal
+        o_MEM_jal        : out std_logic;                     -- Jump and link write back control signal
+        o_MEM_jr         : out std_logic;                     -- Jump return control signal
         o_MEM_branch     : out std_logic;                     -- Branch output from ALU
         o_MEM_PCP4       : out std_logic_vector(31 downto 0); -- PC+4 value
         o_MEM_rs_data_o  : out std_logic_vector(31 downto 0); -- Output from Rs address
@@ -59,14 +59,14 @@ end EX_MEM_pipe;
 
 architecture mixed of EX_MEM_pipe is
     -- s_D
-    signal s_EX_halt_c     : std_logic;                     -- Halt control signal
-    signal s_EX_MemToReg_c : std_logic_vector(1 downto 0);  -- MemToReg control signal
-    signal s_EX_MemWrite_c : std_logic;                     -- Memory write control signal
-    signal s_EX_RegWrite_c : std_logic;                     -- Register write control signal
-    signal s_EX_Jump_c     : std_logic;                     -- Jump control signal
+    signal s_EX_halt       : std_logic;                     -- Halt control signal
+    signal s_EX_MemToReg   : std_logic_vector(1 downto 0);  -- MemToReg control signal
+    signal s_EX_MemWrite   : std_logic;                     -- Memory write control signal
+    signal s_EX_RegWrite   : std_logic;                     -- Register write control signal
+    signal s_EX_Jump       : std_logic;                     -- Jump control signal
     signal s_EX_ext_ctl    : std_logic;                     -- Sign extension control signal
-    signal s_EX_jal_C      : std_logic;                     -- Jump and link write back control signal
-    signal s_EX_jr_c       : std_logic;                     -- Jump return control signal
+    signal s_EX_jal        : std_logic;                     -- Jump and link write back control signal
+    signal s_EX_jr         : std_logic;                     -- Jump return control signal
     signal s_EX_branch     : std_logic;                     -- Branch output from ALU
     signal s_EX_PCP4       : std_logic_vector(31 downto 0); -- PC+4 value
     signal s_EX_rs_data_o  : std_logic_vector(31 downto 0); -- Output from Rs address
@@ -76,14 +76,14 @@ architecture mixed of EX_MEM_pipe is
     signal s_EX_Dmem_Addr  : std_logic_vector(31 downto 0); -- Output from the ALU
     signal s_EX_add1_mux2  : std_logic_vector(31 downto 0); -- Output from Adder 1
     -- s_Q
-    signal s_MEM_halt_c     : std_logic;                     -- Halt control signal
-    signal s_MEM_MemToReg_c : std_logic_vector(1 downto 0);  -- MemToReg control signal
-    signal s_MEM_MemWrite_c : std_logic;                     -- Memory write control signal
-    signal s_MEM_RegWrite_c : std_logic;                     -- Register write control signal
-    signal s_MEM_Jump_c     : std_logic;                     -- Jump control signal
+    signal s_MEM_halt       : std_logic;                     -- Halt control signal
+    signal s_MEM_MemToReg   : std_logic_vector(1 downto 0);  -- MemToReg control signal
+    signal s_MEM_MemWrite   : std_logic;                     -- Memory write control signal
+    signal s_MEM_RegWrite   : std_logic;                     -- Register write control signal
+    signal s_MEM_Jump       : std_logic;                     -- Jump control signal
     signal s_MEM_ext_ctl    : std_logic;                     -- Sign extension control signal
-    signal s_MEM_jal_C      : std_logic;                     -- Jump and link write back control signal
-    signal s_MEM_jr_c       : std_logic;                     -- Jump return control signal
+    signal s_MEM_jal        : std_logic;                     -- Jump and link write back control signal
+    signal s_MEM_jr         : std_logic;                     -- Jump return control signal
     signal s_MEM_branch     : std_logic;                     -- Branch output from ALU
     signal s_MEM_PCP4       : std_logic_vector(31 downto 0); -- PC+4 value
     signal s_MEM_rs_data_o  : std_logic_vector(31 downto 0); -- Output from Rs address
@@ -97,14 +97,14 @@ begin
 
     -- The output of the FF is fixed to s_Q
     -- o_Q <= s_Q
-    o_MEM_halt_c     <= s_MEM_halt_c;     -- Halt control signal
-    o_MEM_MemToReg_c <= s_MEM_MemToReg_c; -- MemToReg control signal
-    o_MEM_MemWrite_c <= s_MEM_MemWrite_c; -- Memory write control signal
-    o_MEM_RegWrite_c <= s_MEM_RegWrite_c; -- Register write control signal
-    o_MEM_Jump_c     <= s_MEM_Jump_c;     -- Jump control signal
+    o_MEM_halt       <= s_MEM_halt;       -- Halt control signal
+    o_MEM_MemToReg   <= s_MEM_MemToReg;   -- MemToReg control signal
+    o_MEM_MemWrite   <= s_MEM_MemWrite;   -- Memory write control signal
+    o_MEM_RegWrite   <= s_MEM_RegWrite;   -- Register write control signal
+    o_MEM_Jump       <= s_MEM_Jump;       -- Jump control signal
     o_MEM_ext_ctl    <= s_MEM_ext_ctl;    -- Sign extension control signal
-    o_MEM_jal_C      <= s_MEM_jal_C;      -- Jump and link write back control signal
-    o_MEM_jr_c       <= s_MEM_jr_c;       -- Jump return control signal
+    o_MEM_jal        <= s_MEM_jal;        -- Jump and link write back control signal
+    o_MEM_jr         <= s_MEM_jr;         -- Jump return control signal
     o_MEM_branch     <= s_MEM_branch;     -- Branch output from ALU
     o_MEM_PCP4       <= s_MEM_PCP4;       -- PC+4 value
     o_MEM_rs_data_o  <= s_MEM_rs_data_o;  -- Output from Rs address
@@ -121,14 +121,14 @@ begin
     process (i_CLK, i_RST, i_WE)
     begin
         if (i_RST = '1') then
-            s_MEM_halt_c     <= '0';         -- Halt control signal
-            s_MEM_MemToReg_c <= b"00";       -- MemToReg control signal
-            s_MEM_MemWrite_c <= '0';         -- Memory write control signal
-            s_MEM_RegWrite_c <= '0';         -- Register write control signal
-            s_MEM_Jump_c     <= '0';         -- Jump control signal
+            s_MEM_halt       <= '0';         -- Halt control signal
+            s_MEM_MemToReg   <= b"00";       -- MemToReg control signal
+            s_MEM_MemWrite   <= '0';         -- Memory write control signal
+            s_MEM_RegWrite   <= '0';         -- Register write control signal
+            s_MEM_Jump       <= '0';         -- Jump control signal
             s_MEM_ext_ctl    <= '0';         -- Sign extension control signal
-            s_MEM_jal_C      <= '0';         -- Jump and link write back control signal
-            s_MEM_jr_c       <= '0';         -- Jump return control signal
+            s_MEM_jal        <= '0';         -- Jump and link write back control signal
+            s_MEM_jr         <= '0';         -- Jump return control signal
             s_MEM_branch     <= '0';         -- Branch output from ALU
             s_MEM_PCP4       <= x"00400000"; -- PC+4 value
             s_MEM_rs_data_o  <= x"00000000"; -- Output from Rs address
@@ -138,14 +138,14 @@ begin
             s_MEM_Dmem_Addr  <= x"00000000"; -- Output from the ALU
             s_MEM_add1_mux2  <= x"00000000"; -- Output from Adder 1
         elsif (rising_edge(i_CLK) and i_WE = '1') then
-            s_MEM_halt_c     <= i_EX_halt_c;     -- Halt control signal
-            s_MEM_MemToReg_c <= i_EX_MemToReg_c; -- MemToReg control signal
-            s_MEM_MemWrite_c <= i_EX_MemWrite_c; -- Memory write control signal
-            s_MEM_RegWrite_c <= i_EX_RegWrite_c; -- Register write control signal
-            s_MEM_Jump_c     <= i_EX_Jump_c;     -- Jump control signal
+            s_MEM_halt       <= i_EX_halt;       -- Halt control signal
+            s_MEM_MemToReg   <= i_EX_MemToReg;   -- MemToReg control signal
+            s_MEM_MemWrite   <= i_EX_MemWrite;   -- Memory write control signal
+            s_MEM_RegWrite   <= i_EX_RegWrite;   -- Register write control signal
+            s_MEM_Jump       <= i_EX_Jump;       -- Jump control signal
             s_MEM_ext_ctl    <= i_EX_ext_ctl;    -- Sign extension control signal
-            s_MEM_jal_C      <= i_EX_jal_C;      -- Jump and link write back control signal
-            s_MEM_jr_c       <= i_EX_jr_c;       -- Jump return control signal
+            s_MEM_jal        <= i_EX_jal;        -- Jump and link write back control signal
+            s_MEM_jr         <= i_EX_jr;         -- Jump return control signal
             s_MEM_branch     <= i_EX_branch;     -- Branch output from ALU
             s_MEM_PCP4       <= i_EX_PCP4;       -- PC+4 value
             s_MEM_rs_data_o  <= i_EX_rs_data_o;  -- Output from Rs address
@@ -155,14 +155,14 @@ begin
             s_MEM_Dmem_Addr  <= i_EX_Dmem_Addr;  -- Output from the ALU
             s_MEM_add1_mux2  <= i_EX_add1_mux2;  -- Output from Adder 1
         else
-            s_MEM_halt_c     <= s_MEM_halt_c;     -- Halt control signal
-            s_MEM_MemToReg_c <= s_MEM_MemToReg_c; -- MemToReg control signal
-            s_MEM_MemWrite_c <= s_MEM_MemWrite_c; -- Memory write control signal
-            s_MEM_RegWrite_c <= s_MEM_RegWrite_c; -- Register write control signal
-            s_MEM_Jump_c     <= s_MEM_Jump_c;     -- Jump control signal
+            s_MEM_halt       <= s_MEM_halt;       -- Halt control signal
+            s_MEM_MemToReg   <= s_MEM_MemToReg;   -- MemToReg control signal
+            s_MEM_MemWrite   <= s_MEM_MemWrite;   -- Memory write control signal
+            s_MEM_RegWrite   <= s_MEM_RegWrite;   -- Register write control signal
+            s_MEM_Jump       <= s_MEM_Jump;       -- Jump control signal
             s_MEM_ext_ctl    <= s_MEM_ext_ctl;    -- Sign extension control signal
-            s_MEM_jal_C      <= s_MEM_jal_C;      -- Jump and link write back control signal
-            s_MEM_jr_c       <= s_MEM_jr_c;       -- Jump return control signal
+            s_MEM_jal        <= s_MEM_jal;        -- Jump and link write back control signal
+            s_MEM_jr         <= s_MEM_jr;         -- Jump return control signal
             s_MEM_branch     <= s_MEM_branch;     -- Branch output from ALU
             s_MEM_PCP4       <= s_MEM_PCP4;       -- PC+4 value
             s_MEM_rs_data_o  <= s_MEM_rs_data_o;  -- Output from Rs address
--- Project2/Pipeline Registers/MEM_WB_pipe.vhd
+++ Project2/Pipeline Registers/MEM_WB_pipe.vhd
@@ -15,59 +15,59 @@ use IEEE.std_logic_1164.all;
 entity MEM_WB_pipe is
     generic (N : integer := 32);
     port (
-        i_CLK            : in std_logic;                     -- Clock input
-        i_RST            : in std_logic;                     -- Reset input
-        i_WE             : in std_logic;                     -- Write enable
-        i_MEM_halt_c     : in std_logic;                     -- Halt control signal
-        i_MEM_MemToReg_c : in std_logic_vector(1 downto 0);  -- MemToReg control signal
-        i_MEM_RegWrite_c : in std_logic;                     -- Register write control signal
-        i_MEM_jal_C      : in std_logic;                     -- Jump and link write back control signal
-        i_MEM_PCP4       : in std_logic_vector(31 downto 0); -- PC+4 value
-        i_MEM_RegWrAddr  : in std_logic_vector(4 downto 0);  -- Write address
-        i_MEM_Dmem_Addr  : in std_logic_vector(31 downto 0); -- Output from the ALU
+        i_CLK           : in std_logic;                     -- Clock input
+        i_RST           : in std_logic;                     -- Reset input
+        i_WE            : in std_logic;                     -- Write enable
+        i_MEM_halt      : in std_logic;                     -- Halt control signal
+        i_MEM_MemToReg  : in std_logic_vector(1 downto 0);  -- MemToReg control signal
+        i_MEM_RegWrite  : in std_logic;                     -- Register write control signal
+        i_MEM_jal       : in std_logic;                     -- Jump and link write back control signal
+        i_MEM_PCP4      : in std_logic_vector(31 downto 0); -- PC+4 value
+        i_MEM_RegWrAddr : in std_logic_vector(4 downto 0);  -- Write address
+        i_MEM_Dmem_Addr : in std_logic_vector(31 downto 0); -- Output from the ALU
         ------------------------------------------------------------------------------------
         -- outputs
         ------------------------------------------------------------------------------------
-        o_WB_halt_c     : out std_logic;                     -- Halt control signal
-        o_WB_MemToReg_c : out std_logic_vector(1 downto 0);  -- MemToReg control signal
-        o_WB_RegWrite_c : out std_logic;                     -- Register write control signal
-        o_WB_jal_C      : out std_logic;                     -- Jump and link write back control signal
-        o_WB_PCP4       : out std_logic_vector(31 downto 0); -- PC+4 value
-        o_WB_RegWrAddr  : out std_logic_vector(4 downto 0);  -- Write address
-        o_WB_Dmem_Addr  : out std_logic_vector(31 downto 0)  -- Output from the ALU
+        o_WB_halt      : out std_logic;                     -- Halt control signal
+        o_WB_MemToReg  : out std_logic_vector(1 downto 0);  -- MemToReg control signal
+        o_WB_RegWrite  : out std_logic;                     -- Register write control signal
+        o_WB_jal       : out std_logic;                     -- Jump and link write back control signal
+        o_WB_PCP4      : out std_logic_vector(31 downto 0); -- PC+4 value
+        o_WB_RegWrAddr : out std_logic_vector(4 downto 0);  -- Write address
+        o_WB_Dmem_Addr : out std_logic_vector(31 downto 0)  -- Output from the ALU
     );
 
 end MEM_WB_pipe;
 
 architecture mixed of MEM_WB_pipe is
     -- s_D
-    signal s_MEM_halt_c     : std_logic;                     -- Halt control signal
-    signal s_MEM_MemToReg_c : std_logic_vector(1 downto 0);  -- MemToReg control signal
-    signal s_MEM_RegWrite_c : std_logic;                     -- Register write control signal
-    signal s_MEM_jal_C      : std_logic;                     -- Jump and link write back control signal
-    signal s_MEM_PCP4       : std_logic_vector(31 downto 0); -- PC+4 value
-    signal s_MEM_RegWrAddr  : std_logic_vector(4 downto 0);  -- Write address
-    signal s_MEM_Dmem_Addr  : std_logic_vector(31 downto 0); -- Output from the ALU
+    signal s_MEM_halt      : std_logic;                     -- Halt control signal
+    signal s_MEM_MemToReg  : std_logic_vector(1 downto 0);  -- MemToReg control signal
+    signal s_MEM_RegWrite  : std_logic;                     -- Register write control signal
+    signal s_MEM_jal       : std_logic;                     -- Jump and link write back control signal
+    signal s_MEM_PCP4      : std_logic_vector(31 downto 0); -- PC+4 value
+    signal s_MEM_RegWrAddr : std_logic_vector(4 downto 0);  -- Write address
+    signal s_MEM_Dmem_Addr : std_logic_vector(31 downto 0); -- Output from the ALU
     -- s_Q
-    signal s_WB_halt_c     : std_logic;                     -- Halt control signal
-    signal s_WB_MemToReg_c : std_logic_vector(1 downto 0);  -- MemToReg control signal
-    signal s_WB_RegWrite_c : std_logic;                     -- Register write control signal
-    signal s_WB_jal_C      : std_logic;                     -- Jump and link write back control signal
-    signal s_WB_PCP4       : std_logic_vector(31 downto 0); -- PC+4 value
-    signal s_WB_RegWrAddr  : std_logic_vector(4 downto 0);  -- Write address
-    signal s_WB_Dmem_Addr  : std_logic_vector(31 downto 0); -- Output from the ALU
+    signal s_WB_halt      : std_logic;                     -- Halt control signal
+    signal s_WB_MemToReg  : std_logic_vector(1 downto 0);  -- MemToReg control signal
+    signal s_WB_RegWrite  : std_logic;                     -- Register write control signal
+    signal s_WB_jal       : std_logic;                     -- Jump and link write back control signal
+    signal s_WB_PCP4      : std_logic_vector(31 downto 0); -- PC+4 value
+    signal s_WB_RegWrAddr : std_logic_vector(4 downto 0);  -- Write address
+    signal s_WB_Dmem_Addr : std_logic_vector(31 downto 0); -- Output from the ALU
 
 begin
 
     -- The output of the FF is fixed to s_Q
     -- o_Q <= s_Q
-    o_WB_halt_c     <= s_WB_halt_c;     -- Halt control signal
-    o_WB_MemToReg_c <= s_WB_MemToReg_c; -- MemToReg control signal
-    o_WB_RegWrite_c <= s_WB_RegWrite_c; -- Register write control signal
-    o_WB_jal_C      <= s_WB_jal_C;      -- Jump and link write back control signal
-    o_WB_PCP4       <= s_WB_PCP4;       -- PC+4 value
-    o_WB_RegWrAddr  <= s_WB_RegWrAddr;  -- Write address
-    o_WB_Dmem_Addr  <= s_WB_Dmem_Addr;  -- Output from the ALU
+    o_WB_halt      <= s_WB_halt;      -- Halt control signal
+    o_WB_MemToReg  <= s_WB_MemToReg;  -- MemToReg control signal
+    o_WB_RegWrite  <= s_WB_RegWrite;  -- Register write control signal
+    o_WB_jal       <= s_WB_jal;       -- Jump and link write back control signal
+    o_WB_PCP4      <= s_WB_PCP4;      -- PC+4 value
+    o_WB_RegWrAddr <= s_WB_RegWrAddr; -- Write address
+    o_WB_Dmem_Addr <= s_WB_Dmem_Addr; -- Output from the ALU
 
     -- This process handles the asyncrhonous reset and
     -- synchronous write. We want to be able to reset 
@@ -76,29 +76,29 @@ begin
     process (i_CLK, i_RST, i_WE)
     begin
         if (i_RST = '1') then
-            s_WB_halt_c     <= '0';         -- Halt control signal
-            s_WB_MemToReg_c <= b"00";       -- MemToReg control signal
-            s_WB_RegWrite_c <= '0';         -- Register write control signal
-            s_WB_jal_C      <= '0';         -- Jump and link write back control signal
-            s_WB_PCP4       <= x"00400004"; -- PC+4 value
-            s_WB_RegWrAddr  <= b"00000";    -- Write address
-            s_WB_Dmem_Addr  <= x"00000000"; -- Output from the ALU
+            s_WB_halt      <= '0';         -- Halt control signal
+            s_WB_MemToReg  <= b"00";       -- MemToReg control signal
+            s_WB_RegWrite  <= '0';         -- Register write control signal
+            s_WB_jal       <= '0';         -- Jump and link write back control signal
+            s_WB_PCP4      <= x"00400004"; -- PC+4 value
+            s_WB_RegWrAddr <= b"00000";    -- Write address
+            s_WB_Dmem_Addr <= x"00000000"; -- Output from the ALU
         elsif (rising_edge(i_CLK) and i_WE = '1') then
-            s_WB_halt_c     <= i_MEM_halt_c;     -- Halt control signal
-            s_WB_MemToReg_c <= i_MEM_MemToReg_c; -- MemToReg control signal
-            s_WB_RegWrite_c <= i_MEM_RegWrite_c; -- Register write control signal
-            s_WB_jal_C      <= i_MEM_jal_C;      -- Jump and link write back control signal
-            s_WB_PCP4       <= i_MEM_PCP4;       -- PC+4 value
-            s_WB_RegWrAddr  <= i_MEM_RegWrAddr;  -- Write address
-            s_WB_Dmem_Addr  <= i_MEM_Dmem_Addr;  -- Output from the ALU
+            s_WB_halt      <= i_MEM_halt;      -- Halt control signal
+            s_WB_MemToReg  <= i_MEM_MemToReg;  -- MemToReg control signal
+            s_WB_RegWrite  <= i_MEM_RegWrite;  -- Register write control signal
+            s_WB_jal       <= i_MEM_jal;       -- Jump and link write back control signal
+            s_WB_PCP4      <= i_MEM_PCP4;      -- PC+4 value
+            s_WB_RegWrAddr <= i_MEM_RegWrAddr; -- Write address
+            s_WB_Dmem_Addr <= i_MEM_Dmem_Addr; -- Output from the ALU
         else
-            s_WB_halt_c     <= s_WB_halt_c;     -- Halt control signal
-            s_WB_MemToReg_c <= s_WB_MemToReg_c; -- MemToReg control signal
-            s_WB_RegWrite_c <= s_WB_RegWrite_c; -- Register write control signal
-            s_WB_jal_C      <= s_WB_jal_C;      -- Jump and link write back control signal
-            s_WB_PCP4       <= s_WB_PCP4;       -- PC+4 value
-            s_WB_RegWrAddr  <= s_WB_RegWrAddr;  -- Write address
-            s_WB_Dmem_Addr  <= s_WB_Dmem_Addr;  -- Output from the ALU
+            s_WB_halt      <= s_WB_halt;      -- Halt control signal
+            s_WB_MemToReg  <= s_WB_MemToReg;  -- MemToReg control signal
+            s_WB_RegWrite  <= s_WB_RegWrite;  -- Register write control signal
+            s_WB_jal       <= s_WB_jal;       -- Jump and link write back control signal
+            s_WB_PCP4      <= s_WB_PCP4;      -- PC+4 value
+            s_WB_RegWrAddr <= s_WB_RegWrAddr; -- Write address
+            s_WB_Dmem_Addr <= s_WB_Dmem_Addr; -- Output from the ALU
         end if;
 
     end process;
